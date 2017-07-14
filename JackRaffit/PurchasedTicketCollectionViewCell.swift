43803d8f02856e238fcc0ad61968bfe59d4e67f6
ashish nakoti <ashish@ashishs-iMac.local>
2017-07-14 18:13:07 +0530

    TicketPurchased Detail

---
+++ JackRaffit/PurchasedTicketCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PurchasedTicketCollectionViewCell.swift
+//  JackRaffit
+//
+//  Created by ashish nakoti on 7/14/17.
+//  Copyright © 2017 Hooman. All rights reserved.
+//
+
+import UIKit
+
+class PurchasedTicketCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblTicketSet: UILabel!
+}
